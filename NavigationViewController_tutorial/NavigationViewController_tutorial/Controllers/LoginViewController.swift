91d80d7d9f4406d0319dad80e6da00c099a75cef
최지우 <cjw@choejiuui-MacBookPro.local>
2023-01-23 17:13:45 +0900

    NavigationViewController 실습

--- NavigationViewController_tutorial/NavigationViewController_tutorial/Controllers/LoginViewController.swift
+++ NavigationViewController_tutorial/NavigationViewController_tutorial/Controllers/LoginViewController.swift
@@ -9,9 +9,14 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    // 뷰가 생성되었을 때
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+        // 상단 네비게이션 바 부분을 히든 처리한다
+        self.navigationController?.isNavigationBarHidden = true
     }
 
 
