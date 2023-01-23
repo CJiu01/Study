cd831d7bf5d479608378edc054205e81558972d1
최지우 <cjw@choejiuui-MacBookPro.local>
2023-01-23 15:22:02 +0900

    autolayout 실습

---
+++ NavigationViewController_tutorial/NavigationViewController_tutorial/Controllers/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NavigationViewController_tutorial
+//
+//  Created by 최지우 on 2023/01/23.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ NavigationViewController_tutorial/NavigationViewController_tutorial/Controllers/RegisterViewController.swift
@@ -0,0 +1,8 @@
+//
+//  RegisterViewController.swift
+//  NavigationViewController_tutorial
+//
+//  Created by 최지우 on 2023/01/23.
+//
+
+import Foundation
