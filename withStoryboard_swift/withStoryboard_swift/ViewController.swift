9e21acb53162226571500ce69ff704245d48fe49
최지우 <cjw@choejiuui-MacBookPro.local>
2023-01-23 21:31:30 +0900

    without storyboard setting

---
+++ withStoryboard_swift/withStoryboard_swift/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  withStoryboard_swift
+//
+//  Created by 최지우 on 2023/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        self.title = "뷰컨트롤러 No 스토리보드"
+        self.view.backgroundColor = .orange
+    }
+
+
+}
+
