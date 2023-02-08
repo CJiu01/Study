40ab85bb00c410b7489bf4638b464ced6d2ef930
최지우 <cjw@choejiuui-MacBookPro.local>
2023-02-08 14:57:27 +0900

    [Feat] tableView dataSource / delegate 완료

---
+++ practice/AdvancedMyPageView/AdvancedMyPageView/MyPageCell.swift
@@ -0,0 +1,23 @@
+//
+//  MyPageCell.swift
+//  AdvancedMyPageView
+//
+//  Created by 최지우 on 2023/02/08.
+//
+
+import UIKit
+
+class MyPageCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- practice/AdvancedMyPageView/AdvancedMyPageView/ViewController.swift
+++ practice/AdvancedMyPageView/AdvancedMyPageView/ViewController.swift
@@ -6,14 +6,79 @@
 //
 
 import UIKit
+import SnapKit
+
+private let cellID = "mypageCell"
 
 class ViewController: UIViewController {
-
+    
+    // MARK: - Properties
+    
+    let tableView = UITableView()
+    
+    
+    
+    // MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // register cell 
+        tableView.register(MyPageCell.self, forCellReuseIdentifier: cellID)
+        
+        // extension 위임
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        configureUI()
+        
     }
-
-
+    
+    
+    
+    
+    // MARK: - Helpers
+    
+    func configureUI() {
+        
+        setViewHierarchy()
+        setConstrains()
+        
+    }
+    
+    func setViewHierarchy(){
+        view.addSubview(tableView)
+    }
+    
+    func setConstrains(){
+        tableView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+//          make.top.bottom.leading.trailing.equalToSuperView()
+        }
+        
+    }
+    
 }
 
+
+extension ViewController : UITableViewDataSource{
+    
+    // Register number of rows
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    // 캐스팅을 통해 cell 등록
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellID, for: indexPath) as! MyPageCell
+        
+        return cell
+    }
+    
+}
+
+
+extension ViewController : UITableViewDelegate {
+    
+}
