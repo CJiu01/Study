7ee235689e16ca98279ff515cfe32ae4484b5aed
최지우 <cjw@choejiuui-MacBookPro.local>
2023-02-23 23:47:18 +0900

    [Feat] collectionView

---
+++ practice/CollectionViewDemo/CollectionViewDemo/DemoCell.swift
@@ -0,0 +1,12 @@
+//
+//  DemoCell.swift
+//  CollectionViewDemo
+//
+//  Created by 최지우 on 2023/02/23.
+//
+
+import UIKit
+
+class DemoCell: UICollectionViewCell {
+    
+}
--- practice/CollectionViewDemo/CollectionViewDemo/ViewController.swift
+++ practice/CollectionViewDemo/CollectionViewDemo/ViewController.swift
@@ -7,9 +7,19 @@
 
 import UIKit
 
+let cellID = "Cell"
+
 class ViewController: UIViewController {
     
     // MARK: - Properties
+    
+    let collectionView: UICollectionView = {
+        let flowlayout = UICollectionViewFlowLayout()
+        let cv = UICollectionView(frame: .zero, collectionViewLayout:   flowlayout)
+        return cv
+    }()
+    
+    
     
     
     
@@ -17,9 +27,67 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        // collectionView의 DataSource와 Delegate를 사용하기 위해 view에게 권한을 부여해야 함.
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        
+        
+        view.addSubview(collectionView)
+        
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        
+        collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        collectionView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+        collectionView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        
+        collectionView.backgroundColor = .yellow
+        
+        // cell 등록
+        collectionView.register(DemoCell.self, forCellWithReuseIdentifier: cellID)
     }
 
 
 }
 
+extension ViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellID, for: indexPath) as! DemoCell
+        
+        cell.backgroundColor = .red
+        return cell
+    }
+    
+    
+}
+
+
+extension ViewController: UICollectionViewDelegate {
+    
+}
+
+
+// cell의 가로/세로 size 지정
+extension ViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: (view.frame.width - 30 - 10) / 4, height: (view.frame.width - 30 - 10) / 4)
+    }
+    
+    // default로 설정된 item 간의 간격 설정
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    
+    // edge item의 공백
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 5, left: 5, bottom: 5, right: 5)
+    }
+    
+}
