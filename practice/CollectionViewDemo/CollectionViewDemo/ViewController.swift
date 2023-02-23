f3437a0f194bdd51811e218e25c986c992c00ffa
최지우 <cjw@choejiuui-MacBookPro.local>
2023-02-23 23:17:38 +0900

    [Setting] setting

---
+++ practice/CollectionViewDemo/CollectionViewDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CollectionViewDemo
+//
+//  Created by 최지우 on 2023/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    
+    
+    // MARK: - Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
